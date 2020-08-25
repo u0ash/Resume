92afbcb9979877290f397b10fe971b03261327d1
Yusef Ashjaee <u0.ashjaee@outlook.com>
2020-08-25 15:54:55 +0430

    Updates

---
+++ precache-manifest.c892e4cd07c06be054dac6e283f65281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fdd5a36ffefee8075c9dc438034f44",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "07d723e7bb819cb98aee",
+    "url": "/Resume/static/css/main.87e6a308.chunk.css"
+  },
+  {
+    "revision": "f1a2f548b816b44d75ff",
+    "url": "/Resume/static/js/2.cd9f819f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Resume/static/js/2.cd9f819f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d723e7bb819cb98aee",
+    "url": "/Resume/static/js/main.85794500.chunk.js"
+  },
+  {
+    "revision": "e738a16e6fa089e35a7d",
+    "url": "/Resume/static/js/runtime-main.cc0ddb9b.js"
+  }
+]);
